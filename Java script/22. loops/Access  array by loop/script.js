384634bbdd83933c01ff32341a70666efefff131
Nishadchowdhury <Nishadhj111@gmail.com>
2022-01-28 23:18:20 +0600

    js add more

---
+++ Java script/22. loops/Access  array by loop/script.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+var ranna = ['mangsho', 'alu', 'peyaj', 'morich', 'rosun', 'gajor', 'tel' ];
+for(var i = 0; i < ranna.length; i++){
+    var element = ranna[i]
+    console.log(element);
+}
