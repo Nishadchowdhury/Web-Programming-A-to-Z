38800677fb86a0c1837068b1d1aa57081a1d9cc3
Nishadchowdhury <Nishadhj111@gmail.com>
2022-01-29 18:18:35 +0600

    function add

---
+++ Java script/29. function/function.js
@@ -0,0 +1,12 @@
+// function declareatin
+function bringSingara(money){
+    console.log("singarar jonno dise", money);
+    console.log('singara den');
+    var singaraPrice= 10;
+    var singaraQuantity= taka / singaraPrice;
+    return singaraQuantity;
+} 
+var taka = 250;
+// call the function 
+var singara = bringSingara(taka);
+console.log( 'ei nao tomar ' + singara + ' ta singara .');
