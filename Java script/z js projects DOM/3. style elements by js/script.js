a65a9b52e6d35f245f6ad3c063a85a4e6ba79e9d
Nishadchowdhury <Nishadhj111@gmail.com>
2022-02-08 01:55:58 +0600

    DOM add

---
+++ Java script/z js projects DOM/3. style elements by js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("helloBroId").style.color ="red";
+
+
+
+const HelloBroId2 = document.getElementById("helloBroId2");
+HelloBroId2.style.backgroundColor = "yellow";
+HelloBroId2.style.borderRadius = "10px";
+HelloBroId2.style.borderRadius = "yellow";
+HelloBroId2.style.textAlign = "center";
+
---
+++ Java script/z js projects DOM/4. change element's data/script.js
@@ -0,0 +1,1 @@
+document.getElementById("hi").innerText = 'im perfactly changed';
