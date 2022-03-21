aa67780235c014b25a18460cc2da81a76f918db2
Nishadchowdhury <Nishadhj111@gmail.com>
2022-03-21 18:55:33 +0600

    hudai

--- React/my-app-2/src/App.js
+++ React/my-app-2/src/App.js
@@ -1,17 +1,11 @@
-import { useState } from 'react';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Menubar from './Components/Menubar/Menubar';
-import Test from './Components/Test/Test';
 
 function App() {
- const countF = () => {
-   console.log('Hello paichio bro');
- }
+  
   return (
     <div className="App">
-      <Menubar></Menubar>
+     
+     
     </div>
   );
 }
--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,39 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
-const singers = [
-                {name:" Dr.Mahfuz " , job : 'Singer'},
-                {name:"Eav Rahaman" , job : 'Singer pro max'},
-               ];
+import Cosmetics from './Components/Cosmetics/Cosmetics';
 
 
 function App() {
   const nayoks = ['Rubel' , 'Bapparaz'];
   return (
     <div className="App">
-      {
-      nayoks.map(nayok => <Person namee={nayok}></Person>)
-      }
-
-      {
-        singers.map(singer => <Person namee={singer.name} job={singer.job} > </Person>)
-      }
-
-    <h5>New Component</h5>
-    <h5>New Component</h5>
-
+      <Cosmetics></Cosmetics>
     </div>
   );
 }
 
-function Person(props) {
-  console.log(props);
-  return (
-    <div className="person">
-  <h1>{props.namee} khan  </h1>
-  <p>job: {props.job}</p>
-  </div>
-  )
-}
+
 
 export default App;
