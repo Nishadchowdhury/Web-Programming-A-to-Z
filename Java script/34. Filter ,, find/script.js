b0419399f5b78e84e9535e85fb712835321892bd
Nishadchowdhury <Nishadhj111@gmail.com>
2022-03-22 09:45:13 +0600

    hudai

---
+++ Java script/34. Filter ,, find/script.js
@@ -0,0 +1,21 @@
+/* const friedns = ['Tom Hanks','Tom Cruise','Tom Brady','Tom Solaiman'];
+
+const fLengths = friedns.map(friedn => friedn.length);
+
+// console.log(fLengths);
+
+
+const allNmaes = products.forEach(names => console.log(names));
+
+ */
+
+const products = [
+    { name: 'water bottle', price: 50, color: 'yellow' },
+    { name: 'mobile phone', price: 15000, color: 'black' },
+    { name: 'smart watch', price: 3000, color: 'black' },
+    { name: 'sticky note', price: 30, color: 'pink' },
+];
+
+const whiteItem = products.find(product => product.color == 'pink');
+console.log(whiteItem);
+
