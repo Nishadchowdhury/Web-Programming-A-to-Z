cdfdc81e92a5cb95d8638a9348ebb99ebb72933c
Nishadchowdhury <Nishadhj111@gmail.com>
2022-07-16 13:41:23 +0600

    advanced js more number method ad

--- Java script/zzz-Advance-JS/String Methods/Number.js
+++ Java script/zzz-Advance-JS/String Methods/Number.js
@@ -21,15 +21,34 @@ var n2 = "10";
 
 
 
-//___________________________________ Number Method
+//___________________________________ Number Method //=> return(string)
+var num = 10
+var numD = 10.16078
+
 // isNaN() //=> (check, is the value is not a number or else?)
 // NaN === Not a Number
 // Infinity === is a value that contain the largest number of js supports (typeOf infinity => number)
-// let nObject = new Number(123);
-
+// let numObject = new Number(123);
 // console.log(55 + NaN); //=> NaN (it's does't contain a value but NaN is Number type data that's it make any number to NaN)
+// toString() //=> used to convert a number to string.
+// toExponential() //=> num.toExponential(2) //=> 2.00e+0 (used to convert a big number of text to a tiny number of text) //=> return(string)
+// toFixed() //=> //=> numD.toFixed(8) //=> 10.16078000 (we can specified the number of decimal numbers) without param => no decimal.
+// toPrecision() //=> numD.toPrecision(8) //=> 10.160780 (it will return the decimal number with the specified length) without param => full num.
+// valueOf() //=> (to get the value of object type number).
+// Number() //=> Number(true) //=> 1 [Number(' 1.9 ') => 1.9, (' 1,9 ')=> NaN, (' 1 9') => NaN ] ,,, (used to conv js variable to number).
+// parseInt() //=> (used to conv a number to a integer) ,,, [parseInt('12 37 87') => 12, parseInt('12.3 taka') => 12 ].
+
+// Number.isSafeInteger() //=> (used to check is it a actual number or string type number)
+var num = 100000000
+// Number.toLocaleString() //=> num.toLocaleString() //=> 100,000,000 (return a number as a string using local language formate)
 
 
+//___________________________________ Number Property //=> return(number)
+// MAX_VALUE //=> (i will return a height number of js can understand)
+// MIN_VALUE //=> (i will return a lowest number of js can understand)
+// POSITIVE_INFINITY //=> (returns positive infinity)
+// NEGATIVE_INFINITY //=> (returns negative infinity)
+// NaN //=> (returns NaN)
 
 
 
